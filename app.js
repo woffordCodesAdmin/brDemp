f556b8d5bbd41c96d6d39a399983e70e72e5bc96
HarlemMauriceBrooklyn <admin_nyc@macbook-pro.myfiosgateway.com>
2022-05-04 09:49:17 -0400

    nw

--- app.js
+++ app.js
@@ -7,25 +7,30 @@ let playerOne = {
   isOnDefense: false,
   advanceToNextRound: false,
   attackEnemy: function () {
-    let currentTarget = document.querySelector('.pTwo');
+    let currentTarget = document.querySelector(".pTwoHp");
     // Find Target
+    let me = document.querySelector(".pWon");
 
-    let me = document.querySelector('.pOne')
-
-    let squareUp = document.createElement('img')
-    squareUp.setAttribute('src','https://www.fightersgeneration.com/characters3/ryu-shoryureppa.gif')
-    squareUp.setAttribute('class','.pOne')
+    let squareUp = document.createElement("img");
+    squareUp.setAttribute(
+      "src",
+      "https://media1.giphy.com/media/l2QE6znHVshMqR5ba/giphy.gif"
+    );
+    squareUp.setAttribute("class", ".pOne");
+    me.replaceWith(squareUp);
     //replace with actionMove img
-
-    me.classList.toggle('')
+    squareUp.classList.toggle("initiateAttack");
     // then keyFrame
+    currentTarget.classList.toggle("shakeAble");
 
+    let newTotal = parseInt((playerTwo.hp -= 100));
+    let pointTotal = document.querySelector(".points2");
+    playerTwo.hp = newTotal;
 
-    currentTarget.classList.toggle('.shakeAble')
-    let boom = document.querySelector('.actionBubble')
-    boom.style.display = "block"
-    // boom Appear
+    let bar = document.querySelector(".pTwoHp");
 
+    bar.style.backgroundColor = "red";
+    displayScore();
   },
   dodgeAttack: function () {
     // animate evasive manuever
@@ -33,88 +38,42 @@ let playerOne = {
 };
 
 let playerTwo = {
-  name: "",
-  image: "",
-  hp: 1000,
-  turnToAttack: true,
-  isOnDefense: false,
-  advanceToNextRound: false,
-  attackEnemy: function () {
-    let currentTarget = document.querySelector();
-    // Find Target
-  },
-  dodgeAttack: function () {
-    // animate evasive manuever
-  },
-};
+    name: "",
+    image: "",
+    hp: 1000,
+    turnToAttack: true,
+    isOnDefense: false,
+    advanceToNextRound: false,
+    attackEnemy: function () {
+      let currentTarget = document.querySelector(".pOneHp");
+      // Find Target
+      let me = document.querySelector(".pToo");
+  
+      let squareUp = document.createElement("img");
+      squareUp.setAttribute(
+        "src",
+        "https://thumbs.gfycat.com/ValuableKindlyEquine-max-1mb.gif"
+      );
+      squareUp.setAttribute("class", ".pTwo");
+      me.replaceWith(squareUp);
+      //replace with actionMove img
+      squareUp.classList.toggle("initiateAttack2");
+      // then keyFrame
+      currentTarget.classList.toggle("shakeAble");
+  
+      let newTotal = parseInt((playerOne.hp -= 100));
+      let pointTotal = document.querySelector(".points");
 
-let levelBossOne = {
-  name: "",
-  image: "",
-  hp: 1000,
-  turnToAttack: true,
-  isOnDefense: false,
-  advanceToNextRound: false,
-  attackEnemy: function () {
-    let currentTarget = document.querySelector();
-    // Find Target
-  },
-  dodgeAttack: function () {
-    // animate evasive manuever
-  },
-};
-
-let levelBossTwo = {
-  name: "",
-  image: "",
-  hp: 1000,
-  turnToAttack: true,
-  isOnDefense: false,
-  advanceToNextRound: false,
-  attackEnemy: function () {
-    let currentTarget = document.querySelector();
-    // Find Target
-  },
-  dodgeAttack: function () {
-    // animate evasive manuever
-  },
-};
-let levelBossThree = {
-  name: "",
-  image: "",
-  hp: 1000,
-  turnToAttack: true,
-  isOnDefense: false,
-  advanceToNextRound: false,
-  attackEnemy: function () {
-    let currentTarget = document.querySelector();
-    // Find Target
-  },
-  dodgeAttack: function () {
-    // animate evasive manuever
-  },
-};
-let ryu = {
-  name: "",
-  image: "",
-  hp: 1000,
-  turnToAttack: true,
-  isOnDefense: false,
-  advanceToNextRound: false,
-  attackEnemy: function () {
-    let currentTarget = document.querySelector();
-    // Find Target
-  },
-  dodgeAttack: function () {
-    // animate evasive manuever
-  },
-  moveOne: function () {
-    //   https://bestanimations.com/media/fire/415150878fire-flames-burning-close-up-animated-gif-image.gif
-  },
-  moveTwo: function () {
-    //  https://www.fightersgeneration.com/characters3/ryu-shoryureppa.gif
-  },
-};
+      playerOne.hp = newTotal;
+  
+      let bar = document.querySelector(".pOneHp");
+      bar.style.backgroundColor = "red";
+      displayScore();
+    },
+    dodgeAttack: function () {
+      // animate evasive manuever
+    },
+  };
 
 // akumaVSken: https://c.tenor.com/EPjiTOGXEZ8AAAAi/street-fighter-ken.gif
 
@@ -124,31 +83,131 @@ let ryu = {
 
 // fightSequence: https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/8cbabdda-80b4-4926-ac0f-1b71cc9d64dd/de5kxig-2f6c3641-83a9-4b01-a809-70c288bcf1cd.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzhjYmFiZGRhLTgwYjQtNDkyNi1hYzBmLTFiNzFjYzlkNjRkZFwvZGU1a3hpZy0yZjZjMzY0MS04M2E5LTRiMDEtYTgwOS03MGMyODhiY2YxY2QuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.PJcEsXxyUM8JSMuDE-1vEIzELfWc0LeE-ZhJZaKXu0g
 
+
 const startGame = () => {
   alert("Welcome To BlahBlah Game");
-};
+  if (playerOne.turnToAttack === true) {
+    // if its p1 turn
+    console.log(" PLAYER ONES TURN~");
+    playerOne.isOnDefense = false;
+    // take off Defense
+    
 
-const startRound = (x, y) => {
-  //   grab both images for players
+    playerOne.turnToAttack =false
+    playerOne.isOnDefense= true
+
+    // end Turn
+  } else  {
+    console.log(" PLAYER TWO= TURN~");
+    playerTwo.turnToAttack = true;
+    playerTwo.isOnDefense = false;
+    
+
+    playerTwo.turnToAttack =false
+    playerTwo.isOnDefense= true
+  }
+
 };
 
 const shakeThaRoom = () => {
   // grab board, players, and title
-
   let board = document.querySelector(".gameboard");
   let title = document.querySelector(".titleMain");
   let player = document.querySelector(".pOne");
   let opponent = document.querySelector(".pTwo");
 
-  let items = [board,title,player,opponent]
-    items.forEach((thing)=>{
-        thing.classList.toggle('shakeAble')
-    })
-
+  let items = [board, title, player, opponent];
+  items.forEach((thing) => {
+    thing.classList.toggle("shakeAble");
+  });
 };
 
+const displayScore = () => {
+  let pOneScore = document.querySelector(".points");
+  pOneScore.innerHTML = `${playerOne.hp}`;
 
-const attack = () => {
+  let pTwoScore = document.querySelector(".points2");
+  pTwoScore.innerHTML = `${playerTwo.hp}`;
+};
+displayScore();
+
+const playerOneAttack = () =>{
+    let body = document.querySelector(".battleZone");
+    body.style.backgroundColor = "lightblue";
+
+    playerOne.turnToAttack = true
+    let currentTarget = document.querySelector(".pTwoHp");
+    // Find Target
+    let me = document.querySelector(".pWon");
+
+    let squareUp = document.createElement("img");
+    squareUp.setAttribute(
+      "src",
+      "https://media1.giphy.com/media/l2QE6znHVshMqR5ba/giphy.gif"
+    );
+    squareUp.setAttribute("class", ".pOne");
+    me.replaceWith(squareUp);
+    //replace with actionMove img
+    squareUp.classList.toggle("initiateAttack");
+    // then keyFrame
+    currentTarget.classList.toggle("shakeAble");
+
+    let newTotal = parseInt((playerTwo.hp -= 100));
+    let pointTotal = document.querySelector(".points2");
+    playerTwo.hp = newTotal;
+
+    let bar = document.querySelector(".pTwoHp");
+
+    bar.style.backgroundColor = "red";
+    displayScore();
     
+}
 
+const playerTwoAttack = () =>{
+    let body = document.querySelector(".battleZone");
+    body.style.backgroundColor = "salmon";
+
+    playerTwo.turnToAttack = true
+    let currentTarget = document.querySelector(".pOneHp");
+    // Find Target
+    let me = document.querySelector(".pToo");
+
+    let squareUp = document.createElement("img");
+    squareUp.setAttribute(
+      "src",
+      "https://thumbs.gfycat.com/ValuableKindlyEquine-max-1mb.gif"
+    );
+    squareUp.setAttribute("class", ".pTwo");
+    me.replaceWith(squareUp);
+    //replace with actionMove img
+    squareUp.classList.toggle("initiateAttack2");
+    // then keyFrame
+    currentTarget.classList.toggle("shakeAble");
+
+    let newTotal = parseInt((playerOne.hp -= 100));
+    let pointTotal = document.querySelector(".points");
+
+    playerOne.hp = newTotal;
+
+    let bar = document.querySelector(".pOneHp");
+    bar.style.backgroundColor = "red";
+    displayScore();
+
+}
+
+const changeMap = ()=>{
+    const map = document.querySelector('.gameboard')
+    map.classList.toggle('newLevel')
+}
+
+
+const removeAir = () =>{
+
+    let floatableThings = []
+
+    let board = document.querySelector('.gameboard')
+    let playerOne = document.querySelector('.pWon')
+    let playerTwo = document.querySelector('.pToo')
+    let title = document.querySelector('')
+    // targetAllFloatingItems p1 p2 title board
 }
